ebc223fdd4ae7ffd20b9d76d520eef1c52433027
Thiarlles Gomes <thiarlles@gmail.com>
2019-12-10 22:20:05 +0000

    Project React Study

---
+++ projeto_react/src/componentes/PrimeiroComponente.jsx
@@ -0,0 +1,9 @@
+//Obrigatório já que é um componente
+import React from 'react'
+
+function primeiro() {
+    //Retornar código JSX 
+    return <h1>Meu primeiro componente</h1>
+}
+
+export default primeiro
---
+++ projeto_react/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import PrimeiroComponente from './componentes/PrimeiroComponente'
+
+const element = document.querySelector("#root")
+
+ReactDOM.render(<PrimeiroComponente />, element)
